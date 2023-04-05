1613cef014953e133f45c2e426adf110c7c7cced
shaik <shaiknaseer@gmail.com>
2023-04-05 14:35:42 +0530

    corrected the url

--- april23/callapache.sh
+++ april23/callapache.sh
@@ -2,9 +2,9 @@
 IP_ADDR= '13.127.225.106'
 while :
 do 
-     curl "https://${IP_ADDR}"
-     curl "https://${IP_ADDR}"
-     curl "https://${IP_ADDR}"
-     curl "https://${IP_ADDR}/employees"
+     curl "http://${IP_ADDR}"
+     curl "http://${IP_ADDR}"
+     curl "http://${IP_ADDR}"
+     curl "http://${IP_ADDR}/employees"
 
 done    
