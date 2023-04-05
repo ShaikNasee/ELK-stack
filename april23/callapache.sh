b9241be904dea02c7f8cb27e31eba1f4acbf8048
shaik <shaiknaseer@gmail.com>
2023-04-05 14:31:04 +0530

    added the shell script to send the requests

---
+++ april23/callapache.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+IP_ADDR= '13.127.225.106'
+while :
+do 
+     curl "https://${IP_ADDR}"
+     curl "https://${IP_ADDR}"
+     curl "https://${IP_ADDR}"
+     curl "https://${IP_ADDR}/employees"
+
+done    
